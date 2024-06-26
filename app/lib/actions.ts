47eb0d9cf9380d6de92df26b48d902c5b51a160d
TripleA <93688885+352005@users.noreply.github.com>
2024-06-26 07:31:30 +0100

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -153,6 +153,8 @@ export async function authenticate(
     await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
+      console.log(error.type);
+      
       switch (error.type) {
         case 'CredentialsSignin':
           return 'Invalid credentials.';
