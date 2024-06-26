78a7d424449e466998b54c0f972600f822a20bc4
TripleA <93688885+352005@users.noreply.github.com>
2024-06-26 07:35:58 +0100

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -153,7 +153,7 @@ export async function authenticate(
     await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
-      console.log(error.type);
+      alert(error.type);
       
       switch (error.type) {
         case 'CredentialsSignin':
